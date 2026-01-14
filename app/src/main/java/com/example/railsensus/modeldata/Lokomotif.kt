24d3d6e9dd8913968e33ea99224381dbca2db99c
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-01-15 03:16:58 +0700

    Menambahkan model data `CreateLokoRequest` dan `CreateLokoResponse`
    Menambahkan data class `CreateLokoRequest` untuk kebutuhan pembuatan atau pembaruan data lokomotif, serta `CreateLokoResponse` yang mendukung serialisasi untuk menangani pesan respon dan ID lokomotif dari API.

--- app/src/main/java/com/example/railsensus/modeldata/Lokomotif.kt
+++ app/src/main/java/com/example/railsensus/modeldata/Lokomotif.kt
@@ -15,3 +15,16 @@ data class StatistikLoko(
     val total: Int,
     val total_dipo: Int
 )
+
+//Create / Update
+data class CreateLokoRequest(
+    val nomor_seri: String,
+    val dipo_induk: String,
+    val status: String? = null
+)
+
+@Serializable
+data class CreateLokoResponse(
+    val message: String,
+    val loko_id: Int
+)
