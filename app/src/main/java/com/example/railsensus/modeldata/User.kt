58db830368dad125423dc8f9061d89a91d1c2a3c
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-01-15 02:11:56 +0700

    Menambahkan model data untuk fitur registrasi
    Menambahkan data class `RegisterRequest`, `RegisterResponse`, dan `UserData` ke dalam file `User.kt` untuk mendukung fungsionalitas registrasi pengguna.

--- app/src/main/java/com/example/railsensus/modeldata/User.kt
+++ app/src/main/java/com/example/railsensus/modeldata/User.kt
@@ -2,6 +2,7 @@ package com.example.railsensus.modeldata
 
 import kotlinx.serialization.Serializable
 
+//Login
 data class LoginRequest(
     val email: String,
     val password: String
@@ -26,4 +27,26 @@ data class UILoginState(
     val isEntryValid: Boolean = false,
     val isLoading: Boolean = false,
     val errorMessage: String? = null
+)
+
+//Register
+data class RegisterRequest(
+    val username: String,
+    val email: String,
+    val password: String,
+    val role: String = "User"
+)
+
+@Serializable
+data class RegisterResponse(
+    val message: String,
+    val user: UserData
+)
+
+@Serializable
+data class UserData(
+    val id: Int,
+    val username: String,
+    val email: String,
+    val role: String
 )
