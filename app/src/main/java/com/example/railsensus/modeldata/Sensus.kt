97382a75d6f9f6386ddc5295299229f723470d7b
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-01-15 03:43:57 +0700

    Menambahkan data class `Sensus` pada model data
    Mengimplementasikan data class `Sensus` yang dianotasi dengan `@Serializable` di dalam berkas `Sensus.kt`. Model ini mencakup properti inti seperti `sensus_id`, `user_id`, `loko_id`, `ka_id`, `trust_score`, dan `waktu_sensus`, serta beberapa properti tambahan dari backend seperti `username`, `nomor_seri`, `nama_ka`, `valid_votes`, dan `invalid_votes`.

--- app/src/main/java/com/example/railsensus/modeldata/Sensus.kt
+++ app/src/main/java/com/example/railsensus/modeldata/Sensus.kt
@@ -1,2 +1,20 @@
 package com.example.railsensus.modeldata
 
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Sensus(
+    val sensus_id: Int,
+    val user_id: Int,
+    val loko_id: Int,
+    val ka_id: Int,
+    val trust_score: Int,
+    val waktu_sensus: String,
+
+    //dari backend
+    val username: String? = null,
+    val nomor_seri: String? = null,
+    val nama_ka: String? = null,
+    val valid_votes: Int? = null,
+    val invalid_votes: Int? = null
+)
