3542e6b2518ac9d27b5ef1e195c078c61a86b9ee
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-01-15 03:33:06 +0700

    Menambahkan data class `Kereta`
    Membuat model data `Kereta` yang menggunakan anotasi `@Serializable` dengan atribut `ka_id`, `nama_ka`, dan `no_ka`.

--- app/src/main/java/com/example/railsensus/modeldata/Kereta.kt
+++ app/src/main/java/com/example/railsensus/modeldata/Kereta.kt
@@ -1,2 +1,10 @@
 package com.example.railsensus.modeldata
 
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Kereta(
+    val ka_id: Int,
+    val nama_ka: String,
+    val no_ka: String,
+)
