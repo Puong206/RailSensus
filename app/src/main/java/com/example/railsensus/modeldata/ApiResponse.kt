5c1753d81b562caf87767a82e3da4f3df767de06
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-01-15 04:35:47 +0700

    Menambahkan data class `ApiResponse`
    Menambahkan model data `ApiResponse` yang dianotasi dengan `@Serializable` untuk menangani respons API. Class ini mencakup properti `message`, `sensus_id`, `loko_id`, `ka_id`, dan `user`.

--- app/src/main/java/com/example/railsensus/modeldata/ApiResponse.kt
+++ app/src/main/java/com/example/railsensus/modeldata/ApiResponse.kt
@@ -1,2 +1,12 @@
 package com.example.railsensus.modeldata
 
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ApiResponse(
+    val message: String,
+    val sensus_id: Int? = null,
+    val loko_id: Int? = null,
+    val ka_id: Int? = null,
+    val user: UserData? = null
+)
