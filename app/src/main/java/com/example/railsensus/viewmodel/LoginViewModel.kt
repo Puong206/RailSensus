288b092e276b7e64cc3f6a59f5d4095b6a11ce38
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-01-15 18:36:16 +0700

    Implementasi dasar `LoginViewModel`
    Menambahkan kelas `LoginViewModel` yang menerima parameter `RepositoriRailSensus`. Implementasi ini menyertakan `_loginState` menggunakan `MutableStateFlow` dengan tipe data `UILoginState` dan mengeksposnya sebagai `StateFlow`.

--- app/src/main/java/com/example/railsensus/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/example/railsensus/viewmodel/LoginViewModel.kt
@@ -1,2 +1,17 @@
 package com.example.railsensus.viewmodel
 
+import androidx.lifecycle.ViewModel
+import com.example.railsensus.modeldata.UILoginState
+import com.example.railsensus.repositori.RepositoriRailSensus
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class LoginViewModel (
+    private val repository: RepositoriRailSensus
+) : ViewModel() {
+
+    //Login State
+    private val _loginState = MutableStateFlow(UILoginState())
+    val loginState: StateFlow<UILoginState> = _loginState.asStateFlow()
+}
