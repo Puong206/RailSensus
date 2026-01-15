e4e7b402c47f0ebb8659822238f06ad567fb5b81
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-01-16 03:02:16 +0700

    Menambahkan objek navigasi `DestinasiSensusEntry`
    Menambahkan file baru `DestinasiSensusEntry.kt` yang mendefinisikan rute navigasi `"entry_sensus"` dan judul `"Tambah Sensus"` untuk halaman entri sensus.

---
+++ app/src/main/java/com/example/railsensus/uicontroller/route/DestinasiSensusEntry.kt
@@ -0,0 +1,6 @@
+package com.example.railsensus.uicontroller.route
+
+object DestinasiSensusEntry : DestinasiNavigasi {
+    override val route = "entry_sensus"
+    override val titleRes = "Tambah Sensus"
+}
