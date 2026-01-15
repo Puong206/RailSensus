1c237309ef920d57f755477dee45a68f02f199f1
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-01-16 03:00:54 +0700

    Menambahkan objek navigasi `DestinasiLokoEntry`
    Menambahkan file baru `DestinasiLokoEntry.kt` yang mendefinisikan rute navigasi `entry_loko` dan judul "Tambah Lokomotif" untuk halaman entri lokomotif.

---
+++ app/src/main/java/com/example/railsensus/uicontroller/route/DestinasiLokoEntry.kt
@@ -0,0 +1,6 @@
+package com.example.railsensus.uicontroller.route
+
+object DestinasiLokoEntry : DestinasiNavigasi {
+    override val route = "entry_loko"
+    override val titleRes = "Tambah Lokomotif"
+}
