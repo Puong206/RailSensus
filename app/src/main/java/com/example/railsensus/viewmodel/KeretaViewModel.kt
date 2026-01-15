73aac07394e8f960f1e137232b202bb3c7c75146
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-01-16 00:39:24 +0700

    Implementasi `KeretaViewModel` dan state manajemen
    Menambahkan kelas `KeretaViewModel` yang menggunakan `StateFlow` untuk mengelola data aplikasi, termasuk daftar kereta (`keretaList`), status form (`keretaFormState`), kereta yang dipilih (`selectedKereta`), statistik, status pemuatan (`isLoading`), dan pesan kesalahan (`errorMessage`). Kelas ini juga mengintegrasikan `RepositoriRailSensus` melalui konstruktor.

--- app/src/main/java/com/example/railsensus/viewmodel/KeretaViewModel.kt
+++ app/src/main/java/com/example/railsensus/viewmodel/KeretaViewModel.kt
@@ -1,13 +1,16 @@
 package com.example.railsensus.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.railsensus.modeldata.Kereta
 import com.example.railsensus.modeldata.StatistikKereta
 import com.example.railsensus.modeldata.UIKeretaState
+import com.example.railsensus.repositori.ApiResult
 import com.example.railsensus.repositori.RepositoriRailSensus
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 
 class KeretaViewModel(
     private val repositori: RepositoriRailSensus): ViewModel() {
@@ -30,4 +33,24 @@ class KeretaViewModel(
 
     private val _errorMessage = MutableStateFlow<String?>(null)
     val errorMessage: StateFlow<String?> = _errorMessage.asStateFlow()
+
+    //load all
+    fun loadAllKereta() {
+        viewModelScope.launch {
+            _isLoading.value = true
+            _errorMessage.value = null
+
+            when (val result = repositori.getAllKereta()) {
+                is ApiResult.Success -> {
+                    _keretaList.value = result.data
+                }
+                is ApiResult.Error -> {
+                    _errorMessage.value = result.message
+                }
+                is ApiResult.Loading -> { }
+            }
+
+            _isLoading.value = false
+        }
+    }
 }
