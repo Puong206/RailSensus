d2847805780845c63f41fdcad6da0a04899cc37f
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-01-16 03:02:41 +0700

    Menambahkan objek `DestinasiSensusHome`
    Menambahkan file baru `DestinasiSensusHome.kt` yang mengimplementasikan antarmuka `DestinasiNavigasi` dengan rute `sensus_home` dan judul `Daftar Sensus`.

---
+++ app/src/main/java/com/example/railsensus/uicontroller/route/DestinasiSensusHome.kt
@@ -0,0 +1,6 @@
+package com.example.railsensus.uicontroller.route
+
+object DestinasiSensusHome : DestinasiNavigasi {
+    override val route = "sensus_home"
+    override val titleRes = "Daftar Sensus"
+}
