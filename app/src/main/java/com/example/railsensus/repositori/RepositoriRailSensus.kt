67a98d2c46877e38df5fb02b0a9dca9229c6c80d
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-01-15 14:55:51 +0700

    Menambahkan fungsi autentikasi pada `RepositoriRailSensus`
    Menambahkan fungsi `login`, `register`, and `getProfile` ke dalam `RepositoriRailSensus.kt`. Fungsi-fungsi ini menggunakan `safeApiCall` untuk melakukan pemanggilan API melalui `ServiceApi` dengan menggunakan model data `LoginRequest` dan `RegisterRequest`.

--- app/src/main/java/com/example/railsensus/repositori/RepositoriRailSensus.kt
+++ app/src/main/java/com/example/railsensus/repositori/RepositoriRailSensus.kt
@@ -1,9 +1,23 @@
 package com.example.railsensus.repositori
 
 import com.example.railsensus.apiservice.ServiceApi
+import com.example.railsensus.modeldata.LoginRequest
+import com.example.railsensus.modeldata.RegisterRequest
 
 class RepositoriRailSensus(
     private val serviceApi: ServiceApi
 ) {
+    //Auth
+    suspend fun login(email: String, password: String) = safeApiCall {
+        serviceApi.login(LoginRequest(email, password))
+    }
 
+    suspend fun register(username: String, email: String, password: String) =
+        safeApiCall {
+            serviceApi.register(RegisterRequest(username, email, password))
+    }
+
+    suspend fun getProfile(token: String) = safeApiCall {
+        serviceApi.getProfile(token)
+    }
 }
