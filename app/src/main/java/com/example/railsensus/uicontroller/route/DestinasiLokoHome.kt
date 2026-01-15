d06a7ed77ffc83113de41f03f6eebbdc75857485
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-01-16 03:01:03 +0700

    Menambahkan file `DestinasiLokoHome.kt`
    Menambahkan objek `DestinasiLokoHome` yang mengimplementasikan antarmuka `DestinasiNavigasi` untuk mendefinisikan rute navigasi "loko_home" dan judul "Daftar Lokomotif".

---
+++ app/src/main/java/com/example/railsensus/uicontroller/route/DestinasiLokoHome.kt
@@ -0,0 +1,6 @@
+package com.example.railsensus.uicontroller.route
+
+object DestinasiLokoHome : DestinasiNavigasi {
+    override val route = "loko_home"
+    override val titleRes = "Daftar Lokomotif"
+}
