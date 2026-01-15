09420347347e89b4f9893969afcb384dc6a566ea
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-01-15 19:50:07 +0700

    Menambahkan fungsi login pada `LoginViewModel`
    Menambahkan implementasi fungsi `login` dan `loadUserProfile` pada `LoginViewModel`. Perubahan ini mencakup pengelolaan status UI saat proses autentikasi (loading, error, sukses), penyimpanan token akses, dan pengambilan profil pengguna menggunakan `viewModelScope` serta penanganan hasil melalui `ApiResult`.

--- app/src/main/java/com/example/railsensus/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/example/railsensus/viewmodel/LoginViewModel.kt
@@ -1,15 +1,18 @@
 package com.example.railsensus.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.railsensus.modeldata.UILoginState
 import com.example.railsensus.modeldata.UIRegisterState
 import com.example.railsensus.modeldata.UserProfile
 import com.example.railsensus.modeldata.isValid
+import com.example.railsensus.repositori.ApiResult
 import com.example.railsensus.repositori.RepositoriRailSensus
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
 class LoginViewModel (
     private val repository: RepositoriRailSensus
@@ -44,5 +47,49 @@ class LoginViewModel (
     fun updateLoginPassword(password: String) {
         _loginState.update { it.copy(loginData = it.loginData.copy(password = password)) }
         validateLogin()
+    }
+
+    private suspend fun loadUserProfile(token: String) {
+        when (val result = repository.getProfile(token)) {
+            is ApiResult.Success -> {
+                _currentUser.value = result.data
+            }
+            is ApiResult.Error -> {
+            }
+            is ApiResult.Loading -> {}
+        }
+    }
+
+
+    fun login() {
+        val loginData = _loginState.value.loginData
+        if (!loginData.isValid()) {
+            _loginState.update { it.copy(errorMessage = "Email dan password tidak boleh kosong")}
+            return
+        }
+        viewModelScope.launch {
+            _loginState.update { it.copy(
+                isLoading = true,
+                errorMessage = null
+            ) }
+
+            when (val result = repository.login(loginData.email, loginData.password)) {
+                is ApiResult.Success -> {
+                    _currentToken.value = result.data.accessToken
+                    loadUserProfile(result.data.accessToken)
+                    _loginState.update { it.copy(
+                        isLoading = false,
+                        errorMessage = null
+                    )}
+                }
+                is ApiResult.Error -> {
+                    _loginState.update { it.copy(
+                        isLoading = false,
+                        errorMessage = result.message
+                    ) }
+                }
+                is ApiResult.Loading -> {}
+            }
+        }
     }
 }
