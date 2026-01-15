c0f5c8c91f77f6e9f4b317412534f127e9508408
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-01-15 11:29:35 +0700

    Mengonfigurasi `ContainerApp` dengan `BASE_URL` dan pengaturan `Json`
    Menambahkan konstanta `BASE_URL` dan mendefinisikan konfigurasi `Json` dengan opsi `ignoreUnknownKeys` dan `coerceInputValues` di dalam kelas `ContainerApp`. Selain itu, menambahkan impor untuk `kotlinx.serialization.json.Json` dan `HttpLoggingInterceptor`.

--- app/src/main/java/com/example/railsensus/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/railsensus/repositori/ContainerApp.kt
@@ -1,11 +1,21 @@
 package com.example.railsensus.repositori
 
+import kotlinx.serialization.json.Json
 import com.example.railsensus.apiservice.ServiceApi
+import okhttp3.logging.HttpLoggingInterceptor
 
 interface AppContainer{
     val serviceApi: ServiceApi
 }
 
 class ContainerApp: AppContainer{
+    private val BASE_URL = "http://10.0.2.2:3000/"
+
+    //Konfigurassi JSON untuk serialization
+    private val json = Json {
+        ignoreUnknownKeys = true
+        coerceInputValues = true
+    }
+
 
 }
