a12b91945cecbe8d4c4d45e7ed4594f91452570e
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-01-15 18:44:25 +0700

    Menambahkan state untuk Token dan Profil Pengguna di `LoginViewModel`
    Menambahkan `_currentToken` dan `_currentUser` menggunakan `MutableStateFlow` beserta properti publiknya ke dalam `LoginViewModel.kt` untuk menyimpan data token dan profil pengguna.

--- app/src/main/java/com/example/railsensus/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/example/railsensus/viewmodel/LoginViewModel.kt
@@ -3,6 +3,7 @@ package com.example.railsensus.viewmodel
 import androidx.lifecycle.ViewModel
 import com.example.railsensus.modeldata.UILoginState
 import com.example.railsensus.modeldata.UIRegisterState
+import com.example.railsensus.modeldata.UserProfile
 import com.example.railsensus.repositori.RepositoriRailSensus
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -19,4 +20,11 @@ class LoginViewModel (
     //Register State
     private val _registerState = MutableStateFlow(UIRegisterState())
     val registerState: StateFlow<UIRegisterState> = _registerState.asStateFlow()
+
+    //Penyimpanan Token
+    private val _currentToken = MutableStateFlow<String?>(null)
+    val currentToken: StateFlow<String?> = _currentToken.asStateFlow()
+
+    private val _currentUser = MutableStateFlow<UserProfile?>(null)
+    val currentUser: StateFlow<UserProfile?> = _currentUser.asStateFlow()
 }
