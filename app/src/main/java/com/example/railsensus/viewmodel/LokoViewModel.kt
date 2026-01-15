45651c739b676d93b2d6347c50e45be0b756fe84
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-01-15 21:07:28 +0700

    Mengimplementasikan `LokoViewModel`
    Menambahkan kelas `LokoViewModel` yang menyertakan dependensi `RepositoriRailSensus` serta mendefinisikan `StateFlow` untuk mengelola daftar data `Lokomotif`.

--- app/src/main/java/com/example/railsensus/viewmodel/LokoViewModel.kt
+++ app/src/main/java/com/example/railsensus/viewmodel/LokoViewModel.kt
@@ -1,2 +1,18 @@
 package com.example.railsensus.viewmodel
 
+import androidx.lifecycle.ViewModel
+import com.example.railsensus.modeldata.Lokomotif
+import com.example.railsensus.repositori.RepositoriRailSensus
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class LokoViewModel(
+    private val repositori: RepositoriRailSensus
+) : ViewModel() {
+    //State
+
+    //List Loko
+    private val _lokoList = MutableStateFlow<List<Lokomotif>>(emptyList())
+    val lokoList: StateFlow<List<Lokomotif>> = _lokoList.asStateFlow()
+}
