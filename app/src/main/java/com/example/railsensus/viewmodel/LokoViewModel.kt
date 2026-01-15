2d647eb3ade63d765021f9539a9c48419b38a9a1
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-01-15 21:17:59 +0700

    Mengimplementasikan `LokoViewModel`
    Menambahkan kelas `LokoViewModel` yang menyertakan dependensi `RepositoriRailSensus` serta mendefinisikan `StateFlow` untuk mengelola daftar data `Lokomotif`.

--- app/src/main/java/com/example/railsensus/viewmodel/LokoViewModel.kt
+++ app/src/main/java/com/example/railsensus/viewmodel/LokoViewModel.kt
@@ -2,6 +2,8 @@ package com.example.railsensus.viewmodel
 
 import androidx.lifecycle.ViewModel
 import com.example.railsensus.modeldata.Lokomotif
+import com.example.railsensus.modeldata.StatistikLoko
+import com.example.railsensus.modeldata.UILokomotifState
 import com.example.railsensus.repositori.RepositoriRailSensus
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -15,4 +17,24 @@ class LokoViewModel(
     //List Loko
     private val _lokoList = MutableStateFlow<List<Lokomotif>>(emptyList())
     val lokoList: StateFlow<List<Lokomotif>> = _lokoList.asStateFlow()
+
+    //create/update
+    private val _lokoFormState = MutableStateFlow(UILokomotifState())
+    val lokoFormState: StateFlow<UILokomotifState> = _lokoFormState.asStateFlow()
+
+    //select
+    private val _selectedLoko = MutableStateFlow<Lokomotif?>(null)
+    val selectedLoko: StateFlow<Lokomotif?> = _selectedLoko.asStateFlow()
+
+    //statistik
+    private val _statistik = MutableStateFlow<StatistikLoko?>(null)
+    val statistik: StateFlow<StatistikLoko?> = _statistik.asStateFlow()
+
+    //loading
+    private val _isLoading = MutableStateFlow(false)
+    val isLoading: StateFlow<Boolean> = _isLoading.asStateFlow()
+
+    //pesan error
+    private val _errorMessage = MutableStateFlow<String?>(null)
+    val errorMessage: StateFlow<String?> = _errorMessage.asStateFlow()
 }
