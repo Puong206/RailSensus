9080ea915f060d78de6071ed00284b1ba8b5e6b2
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-01-16 23:00:25 +0700

    Menambahkan fungsi ekstensi `railSensusApplication` pada `CreationExtras`
    Menambahkan fungsi ekstensi `railSensusApplication` di dalam `ViewModelProvider.kt` untuk mempermudah akses ke instance `RailSensusApp` menggunakan `APPLICATION_KEY`.

--- app/src/main/java/com/example/railsensus/viewmodel/provider/ViewModelProvider.kt
+++ app/src/main/java/com/example/railsensus/viewmodel/provider/ViewModelProvider.kt
@@ -1,2 +1,9 @@
 package com.example.railsensus.viewmodel.provider
 
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import com.example.railsensus.RailSensusApp
+
+fun CreationExtras.railSensusApplication(): RailSensusApp =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as RailSensusApp)
+
